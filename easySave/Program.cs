7c1c7d17f7fe0da8f5cafd0ca38e82e362971db9
Sheridan SHABANI <sheridan.shabani@outlook.fr>
2022-01-17 11:05:25 +0100

    Ajoutez des fichiers projet.

---
+++ easySave/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace easySave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
