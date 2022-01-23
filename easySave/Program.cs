4e6a00553c280134c49afa57eacba55a6f0c1824
Sheridan Shabani <sheridan.shabani@outlook.fr>
2022-01-23 16:56:15 +0100

    Added basic comments - Program.cs

--- easySave/Program.cs
+++ easySave/Program.cs
@@ -1,9 +1,26 @@
-﻿using System;
+﻿/// \file Program.cs
+/// \author Sheridan SHABANI
+/// \author Steven LUCAS
+/// \author Ahmed EL HARIRI
+/// \version 0.1
+/// \date 23/01/2022
 
+using System;
+
+/// <summary>
+/// Main program namespace 
+/// </summary>
 namespace easySave
 {
+    /// <summary>
+    /// Main program class
+    /// </summary>
     class Program
     {
+        /// <summary>
+        ///  Main program method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
