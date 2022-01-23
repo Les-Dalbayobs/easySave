1be090bfe51b70b79609c6da2727cf26c0511b15
Sheridan Shabani <sheridan.shabani@outlook.fr>
2022-01-23 20:05:43 +0100

    Added basic comments - viewModel.cs

--- easySave/ViewModel/viewModel.cs
+++ easySave/ViewModel/viewModel.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿/// \file viewModel.cs
+/// \author Sheridan SHABANI
+/// \author Steven LUCAS
+/// \author Ahmed EL HARIRI
+/// \version 0.1
+/// \date 23/01/2022
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
+/// <summary>
+/// View namespace
+/// </summary>
 namespace easySave.ViewModel
 {
+    /// <summary>
+    /// Class with all project views
+    /// </summary>
     class viewModel
     {
+
     }
 }
--- easySave/Views/view.cs
+++ easySave/Views/view.cs
@@ -1,9 +1,22 @@
-﻿using System;
+﻿/// \file view.cs
+/// \author Sheridan SHABANI
+/// \author Steven LUCAS
+/// \author Ahmed EL HARIRI
+/// \version 0.1
+/// \date 23/01/2022
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
+/// <summary>
+/// 
+/// </summary>
 namespace easySave.Views
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class view
     {
     }
