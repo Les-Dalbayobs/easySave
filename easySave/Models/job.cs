aa1da5fbc9be235b41bc5f6dc0925118a89a64a2
Sheridan SHABANI <sheridan.shabani@outlook.fr>
2022-01-25 14:35:17 +0100

    Creation of the job.cs file

---
+++ easySave/Models/job.cs
@@ -0,0 +1,35 @@
+﻿/// \file job.cs
+/// \author Sheridan SHABANI
+/// \author Steven LUCAS
+/// \author Ahmed EL HARIRI
+/// \version 0.1
+/// \date 25/01/2022
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Models namespace
+/// </summary>
+namespace easySave.Models
+{
+    /// <summary>
+    /// Class allowing the creation of jobs
+    /// </summary>
+    class job
+    {
+        #region attributes
+        #endregion
+
+        #region properties
+        #endregion
+
+        #region constructors
+        #endregion
+
+        #region methodes
+        #endregion
+
+    }
+}
