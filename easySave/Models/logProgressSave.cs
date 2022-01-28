51b22601d9c0770c95a06c2c1a4ba88148ff4208
Sheridan Shabani <sheridan.shabani@outlook.fr>
2022-01-28 18:41:49 +0100

    Adding attributes and properties

--- easySave/Models/logProgressSave.cs
+++ easySave/Models/logProgressSave.cs
@@ -6,5 +6,34 @@ namespace easySave.Models
 {
     class logProgressSave
     {
+        #region attributes
+        string name;
+        string fileSource;
+        string fileTarget;
+        string destPath;
+        string fileSize;
+        string fileTransfertTime;
+        string time;
+
+        #endregion
+
+        #region properties
+        public string Name { get => name; set => name = value; }
+        public string FileSource { get => fileSource; set => fileSource = value; }
+        public string FileTarget { get => fileTarget; set => fileTarget = value; }
+        public string DestPath { get => destPath; set => destPath = value; }
+        public string FileSize { get => fileSize; set => fileSize = value; }
+        public string FileTransfertTime { get => fileTransfertTime; set => fileTransfertTime = value; }
+        public string Time { get => time; set => time = value; }
+
+        #endregion
+
+        #region constructor
+
+        #endregion
+
+        #region methodes
+
+        #endregion
     }
 }
