1e8e739dc5eae83c2ee3c699ed54892d646de26e
Sheridan Shabani <sheridan.shabani@outlook.fr>
2022-01-28 22:39:28 +0100

    Adding the file and the basic elements

---
+++ easySave/Models/logStatut.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace easySave.Models
+{
+    class logStatut
+    {
+        #region atributes
+        string name;
+        string sourceFilePath;
+        string targetFilePath;
+        string state;
+        string totalFilesSize;
+        string nbFilesLeftToDo;
+        string progression;
+        #endregion
+
+        #region properties
+        public string Name { get => name; set => name = value; }
+        public string SourceFilePath { get => sourceFilePath; set => sourceFilePath = value; }
+        public string TargetFilePath { get => targetFilePath; set => targetFilePath = value; }
+        public string State { get => state; set => state = value; }
+        public string TotalFilesSize { get => totalFilesSize; set => totalFilesSize = value; }
+        public string NbFilesLeftToDo { get => nbFilesLeftToDo; set => nbFilesLeftToDo = value; }
+        public string Progression { get => progression; set => progression = value; }
+        #endregion
+
+        #region constructor
+        public logStatut()
+        {
+
+        }
+        #endregion
+
+        #region methods
+
+        #endregion
+
+    }
+}
