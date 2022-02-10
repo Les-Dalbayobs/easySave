c3c55210c23614d3e3ec460834092ee1ae95989f
Sheridan Shabani <sheridan.shabani@outlook.fr>
2022-02-10 19:10:36 +0100

    Adding a copy of all jobs

--- easySave - Graphic/Views/saveMenu.xaml.cs
+++ easySave - Graphic/Views/saveMenu.xaml.cs
@@ -29,7 +29,30 @@ namespace easySave___Graphic.Views
 
             if (this.RadioAllJob.IsChecked == true)
             {
-
+                bool error = false;
+                foreach (var oneJob in mainW.Jobs)
+                {
+                    if (!mainW.SelectedJob.verifExist(mainW.SelectedJob.PathSource))
+                    {
+                        MessageBox.Show("Source path error of the job: " + oneJob.Name);
+                        error = true;
+                        break;
+                    }
+                    if (!mainW.SelectedJob.verifCreateDestination())
+                    {
+                        MessageBox.Show("Destination path error of the job: " + oneJob.Name);
+                        error = true;
+                        break;
+                    }
+                    
+                }
+                if (!error)
+                {
+                    foreach (var oneJob in mainW.Jobs)
+                    {
+                        oneJob.copy();
+                    }
+                }
             }
             else
             {
