2bfb81096e4c293c6a6a738a79868e20ceb83665
Sheridan Shabani <sheridan.shabani@outlook.fr>
2022-02-10 17:14:04 +0100

    Saving one job, ok

--- easySave - Graphic/Views/saveMenu.xaml.cs
+++ easySave - Graphic/Views/saveMenu.xaml.cs
@@ -24,12 +24,34 @@ namespace easySave___Graphic.Views
 
         private void Ok_Click(object sender, RoutedEventArgs e)
         {
+            ViewModel.MainWindowsViewsModel mainW = this.DataContext as ViewModel.MainWindowsViewsModel;
 
+
+            if (this.RadioAllJob.IsChecked == true)
+            {
+
+            }
+            else
+            {
+                if (!mainW.SelectedJob.verifExist(mainW.SelectedJob.PathSource))
+                {
+                    MessageBox.Show("Source path error");
+                }
+                else if (!mainW.SelectedJob.verifCreateDestination())
+                {
+                    MessageBox.Show("Destination path error");
+                }
+                else
+                {
+                    mainW.SelectedJob.copy();
+                }
+            }
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
-
+            this.DialogResult = false;
+            this.Close();
         }
     }
 }
