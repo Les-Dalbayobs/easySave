48284701e9e06fbcbac374b0e4f95af6938f02de
stivluc <steven.lucas@viacesi.fr>
2022-02-10 11:53:42 +0100

    saveMenu design

---
+++ easySave - Graphic/Views/saveMenu.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace easySave___Graphic.Views
+{
+    /// <summary>
+    /// Interaction logic for saveMenu.xaml
+    /// </summary>
+    public partial class saveMenu : Window
+    {
+        public saveMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
