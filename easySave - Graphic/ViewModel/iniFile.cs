920534dfc300fb4300b6a9eda8e577efbb37601e
Sheridan SHABANI <sheridan.shabani@outlook.fr>
2022-02-15 10:48:03 +0100

    add class ini

---
+++ easySave - Graphic/ViewModel/iniFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace easySave___Graphic.ViewModel
+{
+    class iniFile
+    {
+        string path;
+        string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32")]
+        static extern long WritePrivateProfileString(string section, string key, string value, string filePath);
+
+        [DllImport("kernel32")]
+        static extern int GetPrivateProfileString(string section, string key, string Default, StringBuilder retVal, int Size, string filePath);
+
+        public iniFile(string iniPath)
+        {
+            path = new FileInfo(iniPath ?? EXE + ".ini").FullName.ToString();
+        }
+
+        public string read(string key, string section = null)
+        {
+            var retVal = new StringBuilder(255);
+
+            GetPrivateProfileString(section ?? EXE, key, "", retVal, 255, path);
+
+            return retVal.ToString();
+        }
+
+        public void write(string key, string value, string section = null)
+        {
+            WritePrivateProfileString(section ?? EXE, key, value, path);
+        }
+    }
+}
